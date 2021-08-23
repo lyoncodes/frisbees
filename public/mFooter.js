6ef80752fe33b4684aaf62df5f323aa97a7ea0e5
Michael Lyon <lyoncodes@gmail.com>
2021-08-22 19:05:30 -0700

    updates footer link

--- public/mFooter.js
+++ public/mFooter.js
@@ -9,7 +9,7 @@ const Data = {
       <div><slot name="email"/></div>
       <div><slot name="trademark"/></div>
     </div>
-    <a href="https://validator.w3.org/nu/?doc=http://www.bengaubert.com/"><slot name="html_valid"></a>
+    <a href="https://validator.w3.org/nu/?doc=https%3A%2F%2Fpreviousunwelcomesearchservice.lyoncodes.repl.co%2F"><slot name="html_valid"></a>
     <a href="https://jigsaw.w3.org/css-validator/check?uri=referer"><slot name="CSS_valid"></a>
     <a href="https://docs.google.com/document/d/1C9ncBt9n0SlBUoNZJ_YMNIYP1WayiwMrGITuZ-leySo/edit?usp=sharing"><slot name="disclaimer"></a>
     <link rel="stylesheet" href="./styles/style.css">
